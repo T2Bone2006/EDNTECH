547a1f3231ec8acf3c35872036522e3f19a65178
Taylor <taylorbk2006@gmail.com>
2023-11-30 16:48:58 +0000

    idek

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 transition={{duration: 1}}
             > 
             </motion.div>
-            <motion.div className="text-center flex justify-center">
+            <motion.div className="text-center flex justify-center lg:hidden">
                 <motion.h1 className="text-6xl text-white font-medium z-10 w-3/4" >Your Ticket<br/> to <br/><span className="bg-gradient-to-r from-brand-light-blue to-brand-purple text-transparent bg-clip-text font-medium">Business Growth</span></motion.h1>
             </motion.div>
             <motion.div className="absolute bottom-0 -right-48 h-96 w-96 rounded-full bg-gradient-to-br from-brand-light-blue from-25% to-brand-blue to-85% mt-10 blur-md"
